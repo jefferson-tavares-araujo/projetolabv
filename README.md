f2f3a4639d0c75e4c47863f2febd4ed779d3cbbc
@jefferson_tavares <jefferson.tavares@yahoo.com.br>
2022-10-27 15:33:25 -0300

    Projeto Lab V

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# projetolabv
-Laboratorio 5º Semestre
+# Projeto da disciplina Laboratório de Desenvolvimento em Banco de Dados V
+
+## Para executar: 
+* mvn spring-boot:run
+
+## Caso tenha algum erro envolvendo versão de Java:
+* mvn clean
+
+## Para executar testes: 
+* mvn test
+
+## Para executar testes e gerar relatório: 
+* mvn test jacoco:report
+
+Para visualizar os testes, abra no navegador o arquivo "index.html" em "/target/site/jacoco"
+
---
+++ src/test/java/br/gov/sp/fatec/projetolab5/controller/UsuarioControllerIntegrationTest.java
@@ -0,0 +1,46 @@
+package br.gov.sp.fatec.projetolab5.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Rollback
+public class UsuarioControllerIntegrationTest {
+    
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void novoUsuarioTestOk() throws Exception {
+        mvc.perform(post("/usuario")
+            .content("{\"nome\":\"TesteMvc\", \"senha\":\"senha\"}")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").exists());
+    }
+
+    @Test
+    public void buscarPeloIdTestOk() throws Exception {
+        mvc.perform(get("/usuario/{1}", 1L)
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.nome").value("admin"));
+    }
+
+}
